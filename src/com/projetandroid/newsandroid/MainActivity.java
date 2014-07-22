1b747a3aa0c8090982c3148cd6369e735b022b26
Cheick90 <allimamcheick@yahoo.fr>
2014-07-22 16:30:14 +0100

    ListView
    Voici mes code le nom du projet ce Projet-Sur-GitHub.
    Bon j ai un probleme avec les item je n'arrive pas a cree un clique pour
    chaque items qui renvoie au nagivateur du phone.
    merci

---
+++ src/com/projetandroid/newsandroid/MainActivity.java
@@ -0,0 +1,41 @@
+package com.projetandroid.newsandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+		public class MainActivity extends Activity{
+			ListView list ;
+			private String[] mStrings = {
+		            "Maliweb", "RFI", "Maliactu", "TV5 Monde", "France 24",
+		         };
+			
+			public void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				        setContentView(R.layout.main);
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings);
+			final ListView list = (ListView)findViewById(R.id.list);
+			list.setAdapter(adapter);
+			
+			list.setOnItemClickListener(new OnItemClickListener() {
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					String url = "http://www.maliweb.com"; 
+					Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse( url )); 
+			        startActivity(webIntent);
+				}
+
+				
+			});
+
+				        
+		}
+}
