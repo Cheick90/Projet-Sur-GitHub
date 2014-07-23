770dd9d2697443b17764d79fb385d3630f6adf82
Cheick90 <allimamcheick@yahoo.fr>
2014-07-23 13:07:42 +0100

    Revert "Revert "Revert "ListView"""
    This reverts commit 4dbed98698c560873d6f8715af4d31e76c7b019d.

--- src/com/projetandroid/newsandroid/MainActivity.java
+++ src/com/projetandroid/newsandroid/MainActivity.java
@@ -16,9 +16,13 @@ import android.widget.ListView;
 			private String[] mStrings = {
 		            "Maliweb", "RFI", "Maliactu", "TV5 Monde", "France 24",
 		         };
+<<<<<<< HEAD
 			private String[] mUrls = {"http://www.maliweb.net", "http://www.rfi.fr", "http://www.maliactu.com",
 					"http://www.france24.fr", "http://www.tv5monde.org"};
 		         
+=======
+			
+>>>>>>> parent of c6bb3a6... Revert "ListView"
 			public void onCreate(Bundle savedInstanceState) {
 				super.onCreate(savedInstanceState);
 				        setContentView(R.layout.main);
@@ -30,11 +34,39 @@ import android.widget.ListView;
 				@Override
 				public void onItemClick(AdapterView<?> parent, View view,
 						int position, long id) {
+<<<<<<< HEAD
 					
-					    String url = mUrls[position];
-						Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-						startActivity(webIntent);
+					position=1;
+				    String url_maliweb = mUrls[1]; 
+					Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse( url_maliweb )); 
+			        startActivity(webIntent);
+			        
+			        position=2;
+			        String url_rfi = mUrls[2]; 
+					Intent webIntent1 = new Intent(Intent.ACTION_VIEW, Uri.parse( url_rfi )); 
+			        startActivity(webIntent1);
+			        
+			        position=3;
+			        String url_maliactu = mUrls[3]; 
+					Intent webIntent2 = new Intent(Intent.ACTION_VIEW, Uri.parse( url_maliactu )); 
+			        startActivity(webIntent2);
+			        
+			        position=4;
+			        String url_france24 = mUrls[4]; 
+					Intent webIntent4 = new Intent(Intent.ACTION_VIEW, Uri.parse( url_france24 )); 
+			        startActivity(webIntent4);
+			        
+			        position=5;
+			        String url_tv5monde = mUrls[5]; 
+					Intent webIntent3 = new Intent(Intent.ACTION_VIEW, Uri.parse( url_tv5monde )); 
+			        startActivity(webIntent3);
+			        
 			       
+=======
+					String url = "http://www.maliweb.com"; 
+					Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse( url )); 
+			        startActivity(webIntent);
+>>>>>>> parent of c6bb3a6... Revert "ListView"
 				}
 
 				
